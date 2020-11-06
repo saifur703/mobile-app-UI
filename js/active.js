ad71702de13de779863d2b8a123c51c1426dea65
Saifur Rahman <saifur703@gmail.com>
2020-11-06 19:26:36 +0600

    remove unnecessary jquery code

--- js/active.js
+++ js/active.js
@@ -1,11 +1,6 @@
 ;
 (function ($) {
     $(document).ready(function () {
-        $('.footer a').click(function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            $('#hiddenDiv').show();
-            window.open(this.href, '_self');
-        });
+        
     });
 })(jQuery);
